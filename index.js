d3de8ea7fea441091a1a0a26eb7888d8e556bffa
Loise <loise.wambui@student.moringaschool.com>
2022-12-14 00:08:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  //split it to new array
+  //reverse the new array
+  //join array to string
+
+  return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  return word === reversedWord;
+}
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
@@ -20,6 +22,22 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("come"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("1"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
 }
 
 module.exports = isPalindrome;
